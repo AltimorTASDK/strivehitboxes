e029e91988d8309803878758c3f6160f56a119bd
AltimorTASDK <altimormc@gmail.com>
2021-06-16 01:40:09 -0400

    The injector now checks for updates

--- striveinjector/main.cpp
+++ striveinjector/main.cpp
@@ -1,3 +1,8 @@
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "cpp-httplib/httplib.h"
+
+#include <json/json.h>
+
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <Psapi.h>
@@ -5,6 +10,7 @@
 
 const auto *exe = "GGST-Win64-Shipping.exe";
 const auto *dll = "strivehitboxes.dll";
+const auto *version = "v1.1";
 
 bool elevate_privileges()
 {
@@ -31,6 +37,80 @@ bool elevate_privileges()
 	}
 
 	return true;
+}
+
+bool get_newest_version(std::string *out)
+{
+	httplib::Client client("https://api.github.com");
+	const auto result = client.Get("/repos/AltimorTASDK/strivehitboxes/tags");
+
+	if (result->status != 200)
+		return false;
+
+	const auto body = result->body;
+
+	Json::CharReaderBuilder builder;
+	const auto reader = builder.newCharReader();
+
+	Json::Value root;
+	if (!reader->parse(body.c_str(), body.c_str() + body.length(), &root, nullptr))
+		return false;
+
+	*out = root[0]["name"].asString();
+	return true;
+}
+
+int version_compare(const std::string &a, const std::string &b)
+{
+	if (a.empty() || b.empty())
+		return 0;
+
+	// Skip "v" version prefix
+	size_t index_a = a[0] == 'v' ? 1 : 0;
+	size_t index_b = b[0] == 'v' ? 1 : 0;
+
+	while (true) {
+		const auto num_a = strtol(&a[index_a], nullptr, 10);
+		const auto num_b = strtol(&b[index_b], nullptr, 10);
+
+		if (num_a > num_b)
+			return 1;
+		else if (num_a < num_b)
+			return -1;
+
+		index_a = a.find('.', index_a);
+		index_b = b.find('.', index_b);
+
+		if (index_a != std::string::npos && index_b == std::string::npos)
+			return 1;
+		else if (index_a == std::string::npos && index_b != std::string::npos)
+			return -1;
+		else if (index_a == std::string::npos && index_b == std::string::npos)
+			return 0;
+
+		index_a++;
+		index_b++;
+	}
+}
+
+void check_version()
+{
+	std::string newest;
+	if (!get_newest_version(&newest)) {
+		std::cerr << "Failed to check for updates." << std::endl;
+		return;
+	}
+
+	if (version_compare(version, newest) >= 0)
+		return;
+
+	std::cout << "A newer version (" << newest << ") of this tool is available at ";
+	std::cout << "https://github.com/AltimorTASDK/strivehitboxes/releases" << std::endl;
+	std::cout << "Your version: " << version << std::endl;
+	std::cout << std::endl;
+	std::cout << "Press enter to continue anyways." << std::endl;
+
+	std::cin.get();
 }
 
 bool inject()
@@ -93,6 +173,8 @@ bool inject()
 
 int main()
 {
+	check_version();
+
 	const auto result = inject();
 	Sleep(2000);
 	return result ? 0 : 1;
